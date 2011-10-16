d91d965ab8aec15c5f584dc3d136b772812dcbcc
Olivier Nolbert <o.nolbert@jiliko.net>
2011-10-16 17:10:44 +0200

    v1.7.0

--- plugins/k2/css4k2/css4k2.php
+++ plugins/k2/css4k2/css4k2.php
@@ -1,72 +1,52 @@
 <?php
-/*------------------------------------------------------------------------
-
-# plg_onecssperk2template - CSS4K2 K2 plugin
-
-# ------------------------------------------------------------------------
-
-# author    Jiliko.net
-
-# copyright Copyright (C) 2010 Jiliko.net. All Rights Reserved.
-
-# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
-
-# Websites: http://www.jiliko.net
-
-# Technical Support:  Forum - http://www.jiliko.net/forum
-
--------------------------------------------------------------------------*/
 
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
 JLoader::register('K2Plugin',JPATH_ADMINISTRATOR.DS.'components'.DS.'com_k2'.DS.'lib'.DS.'k2plugin.php');
 
-class plgK2OneCssPerK2Template extends K2Plugin {
+class plgK2OCss4K2 extends K2Plugin {
 
 	// Some params
-	var $pluginName = 'oneCssPerK2Template';
-	var $pluginNameHumanReadable = 'One Css Per K2 Template';
+	var $pluginName = 'Css4K2';
+	var $pluginNameHumanReadable = 'Css4K2';
 
-	function plgK2OneCssPerK2Template(&$subject, $params) {
-		parent::__construct($subject, $params);
+	public function __construct(& $subject, $config) {
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
 	}
 	
 	function onK2PrepareContent( & $item, & $params, $limitstart) {
-		global $mainframe;
+		$app = JFactory::getApplication();
 	}
 
 	function onK2AfterDisplay( & $item, & $params, $limitstart) {
-		global $mainframe;
+		$app = JFactory::getApplication();
 		return '';
 	}
 
 	function onK2BeforeDisplay( & $item, & $params, $limitstart) {
-		global $mainframe;
+		$app = JFactory::getApplication();
 		return '';
 	}
 
 	function onK2AfterDisplayTitle( & $item, & $params, $limitstart) {
-		global $mainframe;
+		$app = JFactory::getApplication();
 		return '';
 	}
 
 	function onK2BeforeDisplayContent( & $item, & $params, $limitstart) {
-		global $mainframe;
+		$app = JFactory::getApplication();
 		return '';
 	}
 
 	function onK2AfterDisplayContent( & $item, & $params, $limitstart) {
-		global $mainframe;
-		
-		//We load the plugin parameters
-		$plugin =& JPluginHelper::GetPlugin('k2', 'onecssperk2template');
-		$pluginParams = new JParameter($plugin->params);
+		$app = JFactory::getApplication();
 		
 		// Call loadCss function if we're in the k2 item view
-		$view=JRequest::getCmd('view');
+		$view = JRequest::getCmd('view');
 		
-		if ($pluginParams->get('multiCss',0))
+		if ($this->params->get('multiCss',0))
 			$this->loadCss($item->params);
 		else
 			$this->loadCss($params);
@@ -75,31 +55,31 @@ class plgK2OneCssPerK2Template extends K2Plugin {
 	}
 
 	function onK2CategoryDisplay( & $category, & $params, $limitstart) {
-		global $mainframe;
+		$app = JFactory::getApplication();
 		
 		// Call loadCss function if we're in the k2 itemlist view
-		$view=JRequest::getCmd('view');
+		$view = JRequest::getCmd('view');
 		
-		if($view == 'itemlist')
+		if ($view == 'itemlist')
 			$this->loadCss($params);
 			
 		return '';
 	}
 
 	function onK2UserDisplay( & $user, & $params, $limitstart) {
-		global $mainframe;
+		$app = JFactory::getApplication();
 		
 		// Call loadCss function if we're in the k2 itemlist view
-		$view=JRequest::getCmd('view');
+		$view = JRequest::getCmd('view');
 		
-		if($view == 'itemlist')
+		if ($view == 'itemlist')
 			$this->loadCss($params);
 			
 		return '';
 	}
 
 	function onK2SherpaSearchDisplay( & $search, & $params, $limitstart) {
-		global $mainframe;
+		$app = JFactory::getApplication();
 		
 		$this->loadCss($params);
 			
@@ -107,7 +87,7 @@ class plgK2OneCssPerK2Template extends K2Plugin {
 	}
 	
 	function loadCss($params) {
-		global $mainframe;
+		$app = JFactory::getApplication();
 	
 		jimport('joomla.filesystem.file');
 		
@@ -120,20 +100,15 @@ class plgK2OneCssPerK2Template extends K2Plugin {
 
 		//We add the css file to the head of the document.
 		//Testing where to get the custom K2 template css
-		if (JFile::exists(JPATH_SITE.DS.'templates'.DS.$mainframe->getTemplate().DS.'html'.DS.'com_k2'.DS.$theme.DS.$theme.'_style.css'))
-			$doc->addStyleSheet(JURI::base().'templates/'.$mainframe->getTemplate().'/html/com_k2/'.$theme.'/'.$theme.'_style.css');
-		elseif (JFile::exists(JPATH_SITE.DS.'templates'.DS.$mainframe->getTemplate().DS.'html'.DS.'com_k2'.DS.'templates'.DS.$theme.DS.$theme.'_style.css'))
-			$doc->addStyleSheet(JURI::base().'templates/'.$mainframe->getTemplate().'/html/com_k2/templates/'.$theme.'/'.$theme.'_style.css');
+		if (JFile::exists(JPATH_SITE.DS.'templates'.DS.$app->getTemplate().DS.'html'.DS.'com_k2'.DS.$theme.DS.$theme.'_style.css'))
+			$doc->addStyleSheet(JURI::base().'templates/'.$app->getTemplate().'/html/com_k2/'.$theme.'/'.$theme.'_style.css');
+		elseif (JFile::exists(JPATH_SITE.DS.'templates'.DS.$app->getTemplate().DS.'html'.DS.'com_k2'.DS.'templates'.DS.$theme.DS.$theme.'_style.css'))
+			$doc->addStyleSheet(JURI::base().'templates/'.$app->getTemplate().'/html/com_k2/templates/'.$theme.'/'.$theme.'_style.css');
 		elseif (JFile::exists(JPATH_SITE.DS.'components'.DS.'com_k2'.DS.'templates'.DS.$theme.DS.$theme.'_style.css'))
-			$doc->addStyleSheet(JURI::base().'components/com_k2/templates/'.$theme.'/'.$theme.'_style.css');
-			
-		//We load the plugin parameters
-		$plugin =& JPluginHelper::GetPlugin('k2', 'onecssperk2template');
-		$pluginParams = new JParameter($plugin->params);
-	  
+			$doc->addStyleSheet(JURI::base().'components/com_k2/templates/'.$theme.'/'.$theme.'_style.css');	  
 	  
 		//If we DON'T want to keep the k2 css loaded
-		if(!$pluginParams->get('keepk2css',1)) {
+		if(!$this->params->get('keepk2css',1)) {
 			//We load the head data of the document in an array
 			$tabHead = $doc->getHeadData();
 
